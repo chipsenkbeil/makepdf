f2c885262eb3a0da1c290dd44825127e4b25e62b
Chip Senkbeil <chip@senkbeil.org>
2024-09-16 18:39:13 -0500

    Rename prefix `makepdf:` to `builtin:` (#2)
    Summary: Renames prefix `makepdf:` used to detect a builtin script to use `builtin:` instead. Also changes the --script default to be `makepdf.lua` instead of `builtin:panda`.
    
    Test Plan:
    
    ```
    # When we have a makepdf.lua file
    makepdf make
    
    # To test the builtin file
    makepdf make --script builtin:panda
    ```

--- assets/scripts/panda.lua
+++ assets/scripts/panda.lua
@@ -4,25 +4,15 @@
 -- Recreation of the planner that has a panda.
 -------------------------------------------------------------------------------
 
-local f = pdf.font
-local o = pdf.object
-local u = pdf.utils
-
-for _, id in ipairs(f.ids()) do
-    local is_fallback = f.fallback() == id
-    print("Font " .. id .. (is_fallback and " (fallback)" or ""))
-end
-
 pdf.page.fill_color = "#999999"
 
-print("hello world")
 pdf.hooks.on_monthly_page(function(page)
-    page.push(o.rect({
+    page.push(pdf.object.rect({
         { 0,  0 },
         { 50, 50 },
     }))
 
-    page.push(o.text({
+    page.push(pdf.object.text({
         { 0, pdf.page.height - 25 },
         text = "Page " .. page.id,
     }))
