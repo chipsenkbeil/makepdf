24198c3d80e18f32eaab02976a702131e34c4205
senkwich@1.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.ip6.arpa <>
2024-09-16 22:18:49 -0500

    Refactor "pdf" global name into constant
    Summary:
    
    Test Plan:

--- src/constants.rs
+++ src/constants.rs
@@ -1,6 +1,9 @@
 /// Default font to use.
 pub const DEFAULT_FONT: &[u8] = include_bytes!("../assets/fonts/JetBrainsMono-Regular.ttf");
 
+/// Name of global variable representing PDF interface.
+pub const GLOBAL_PDF_VAR_NAME: &str = "pdf";
+
 /// Internal scripts available to be run.
 pub static SCRIPTS: phf::Map<&'static str, &[u8]> = phf::phf_map! {
     "example" => include_bytes!("../assets/scripts/example.lua"),
